af0debcc556676abc073fc7b6e36f07360ae13e8
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-08-21 18:56:12 +0530

    Add repo and repoitem component

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,17 +1,21 @@
 import React, { Fragment, Component } from 'react';
 import { Link } from 'react-router-dom';
+import Repos from '../repos/Repos';
 import Spinner from '../layout/Spinner';
 import PropTypes from 'prop-types';
 
 class User extends Component {
 	componentDidMount() {
 		this.props.getUser(this.props.match.params.login);
+		this.props.getUserRepos(this.props.match.params.login);
 	}
 
 	static propTypes = {
 		loading: PropTypes.bool,
 		user: PropTypes.object.isRequired,
-		getUser: PropTypes.func.isRequired
+		getUser: PropTypes.func.isRequired,
+		getUserRepos: PropTypes.func.isRequired,
+		repos: PropTypes.array.isRequired
 	};
 
 	render() {
@@ -30,7 +34,7 @@ class User extends Component {
 			hireable,
 			company
 		} = this.props.user;
-		const { loading } = this.props;
+		const { loading, repos } = this.props;
 
 		if (loading) {
 			return <Spinner />;
@@ -97,6 +101,7 @@ class User extends Component {
 					<div className='badge badge-light'>Public Repos: {public_repos}</div>
 					<div className='badge badge-dark'>Public Gists: {public_gists}</div>
 				</div>
+				<Repos repos={repos} />
 			</Fragment>
 		);
 	}
