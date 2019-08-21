a426a715000a0fd7a46ad6ba1de8f8a34320eb15
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-08-21 14:55:42 +0530

    Add a basic grid of users

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,15 +1,8 @@
 import React, { Component } from 'react';
 
 class UserItem extends Component {
-	state = {
-		login: 'mojombo',
-		id: 'id',
-		avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-		html_url: 'https://github.com/mojombo'
-	};
-
 	render() {
-		const { login, avatar_url, html_url } = this.state;
+		const { login, avatar_url, html_url } = this.props.user;
 		return (
 			<div className='card text-center'>
 				<img src={avatar_url} alt='user' className='round-img' style={{ width: '60px' }} />
